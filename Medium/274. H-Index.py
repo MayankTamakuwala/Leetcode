28caf776b31b2ca2190609439442bd43a6689c63
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-10-15 01:59:11 -0400

    Create 274. H-Index.py

---
+++ Medium/274. H-Index.py
@@ -0,0 +1,20 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        
+        # Solution 1
+
+        # citations.sort()
+        # n, i = len(citations), 1
+        # while i <= n:
+        #     if citations[n-i] < i: break
+        #     i += 1
+        # return i-1
+
+        # Solution 2
+
+        res = 1
+        for i in range(len(citations)):
+            if citations[i] < res:
+                break
+            res += 1
+        return res-1
