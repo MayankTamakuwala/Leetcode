6615aa6abaa36c3d6bba3cc442053d6e62824f07
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-10-21 17:21:35 -0400

    Create 207. Course Schedule.py

---
+++ Medium/207. Course Schedule.py
@@ -0,0 +1,33 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        if len(prerequisites) == 0:
+            return True
+
+        courseMap = collections.defaultdict(list)
+        track = set()
+        for i in prerequisites:
+            courseMap[i[0]].append(i[1])
+
+        visited = [False] * numCourses
+
+        def dfs(course):
+            if course in track:
+                return False
+
+            if not courseMap[course]:
+                return True
+
+            track.add(course)
+
+            for c in courseMap[course]:
+                if not dfs(c):
+                    return False
+            track.remove(course)
+            courseMap[course] = []
+            return True
+
+
+        for k in range(numCourses):
+            if not dfs(k):
+                return False
+        return True
