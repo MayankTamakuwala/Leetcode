4e08c5817f0907b27bffa6121abb269d7bfae84d
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-10-25 15:21:07 -0400

    Create 1497. Check If Array Pairs Are Divisible by k.py

---
+++ Medium/1497. Check If Array Pairs Are Divisible by k.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canArrange(self, arr: List[int], k: int) -> bool:
+        if k == 1:
+            return True
+
+        track = defaultdict(int)
+
+        for i in arr:
+            rem = ((i % k) + k) % k
+            track[rem] += 1
+
+        if track[0] % 2 != 0:
+            return False
+
+        for i in range(1,(k+1)//2):
+            diff = k - i
+            if track[diff] != track[i]:
+                return False
+
+        return True
