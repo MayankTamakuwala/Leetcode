22db35e04c81f78eff0f9e1472242614c91c5bb8
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-10-27 16:11:46 -0400

    Update 1405. Longest Happy String.py

--- Medium/1405. Longest Happy String.py
+++ Medium/1405. Longest Happy String.py
@@ -4,17 +4,14 @@ class Solution:
 
         res = ""
         while True:
-            if "aaa" in res and "bbb" in res and "ccc" in res:
-                break
             first, second, third = sorted(track, key=lambda x: x[1], reverse = True)
 
-            if (res[-2:] == first[0] * 2 and second[1] <= 0 and third[1] <= 0) or (first[1] <= 0):
+            if (res[-2:] == first[0] * 2 and second[1] <= 0) or (first[1] <= 0):
                 break
 
             if res[-2:] == first[0] * 2:
-                if second[1] > 0:
-                    res += second[0]
-                    second = [second[0], second[1] - 1]
+                res += second[0]
+                second = [second[0], second[1] - 1]
             else:
                 res += first[0]
                 first = [first[0], first[1] - 1]
