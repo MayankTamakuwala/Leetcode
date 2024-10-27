9b735ef95e5f99463412f05e51b3669e3a5db251
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-10-27 14:25:28 -0400

    Update 274. H-Index.py

--- Medium/274. H-Index.py
+++ Medium/274. H-Index.py
@@ -1,6 +1,6 @@
 class Solution:
     def hIndex(self, citations: List[int]) -> int:
-        
+
         # Solution 1
 
         # citations.sort()
@@ -12,6 +12,7 @@ class Solution:
 
         # Solution 2
 
+        citations.sort(reverse = True)
         res = 1
         for i in range(len(citations)):
             if citations[i] < res:
