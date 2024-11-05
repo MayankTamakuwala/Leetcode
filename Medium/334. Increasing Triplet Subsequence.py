e94244a3ff99ad09e94c698c96e0b61a8f574d3e
Mayank Tamakuwala <77758563+MayankTamakuwala@users.noreply.github.com>
2024-11-04 23:52:13 -0500

    Create 334. Increasing Triplet Subsequence.py

---
+++ Medium/334. Increasing Triplet Subsequence.py
@@ -0,0 +1,12 @@
+class Solution:
+    def increasingTriplet(self, nums: List[int]) -> bool:
+        min1 = min2 = float('inf')
+
+        for i in nums:
+            if i <= min1:
+                min1 = i
+            elif i <= min2:
+                min2 = i
+            else:
+                return True
+        return False
