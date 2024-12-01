51cd07e38a7e542d6b8890b75d12a4c3e735f6d2
Mayank Tamakuwala <maytamaku.saidhwar@gmail.com>
2024-12-01 16:51:57 -0500

    Create 1346. Check If N and Its Double Exist.py

---
+++ Easy/1346. Check If N and Its Double Exist.py
@@ -0,0 +1,10 @@
+class Solution:
+    def checkIfExist(self, arr: List[int]) -> bool:
+        mySet = set()
+        for n in arr:
+            if n in mySet:
+                return True
+            else:
+                mySet.add(n / 2)
+                mySet.add(n * 2)
+        return False
